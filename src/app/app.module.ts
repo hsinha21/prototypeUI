bedf598aeca49b8a5041ccb14f3e31e678c6a187
Harshit Sinha <hsinha21@gmail.com>
2020-09-20 17:18:13 +0530

    for build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,26 +11,6 @@ import { NgIdleKeepaliveModule } from '@ng-idle/keepalive';
 import { HomeComponent } from './Components/home/home.component';
 import { AddTeamEstimationComponent } from './Components/add-team-estimation/add-team-estimation.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-// Material Import Start
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatInputModule } from '@angular/material/input';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatCardModule } from '@angular/material/card';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatMenuModule } from '@angular/material/menu';
 import { HttpClientModule } from '@angular/common/http';
 
 
@@ -43,10 +23,10 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule, BrowserAnimationsModule, HttpClientModule, NgIdleKeepaliveModule.forRoot(),
-    AppRoutingModule, MatCardModule, MatSelectModule, MatTooltipModule, FormsModule, ReactiveFormsModule, MatInputModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatProgressSpinnerModule, MatSlideToggleModule, MatCheckboxModule, MatRadioModule, MatAutocompleteModule, MatDatepickerModule, MatNativeDateModule, MatProgressBarModule, MatMenuModule
+    AppRoutingModule,FormsModule, ReactiveFormsModule
   ],
   exports: [
-    MatCardModule, MatSelectModule, MatTooltipModule, FormsModule, ReactiveFormsModule, MatInputModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatProgressSpinnerModule, MatSlideToggleModule, MatCheckboxModule, MatRadioModule, MatAutocompleteModule, MatDatepickerModule, MatNativeDateModule, MatProgressBarModule, MatMenuModule
+    FormsModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
